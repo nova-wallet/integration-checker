20eedc1bb1874c3d6377cdafa1c3d0657a8d88bb
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2022-01-24 13:49:44 +0300

    Add shiden network to list

--- test/testData.ts
+++ test/testData.ts
@@ -1,3 +1,3 @@
-const project = ['wallet-acala', 'wallet-altair', 'wallet-astar', 'wallet-basilisk', 'wallet-bifrost', 'wallet-bit-country', 'wallet-calamari', 'wallet-clover', 'edgeware', 'wallet-karura', 'wallet-khala', 'wallet-kilt', 'kusama', 'wallet-moonbeam', 'wallet-moonriver', 'wallet-parallel', 'wallet-parallel-heiko', 'polkadot', 'wallet-quartz', 'wallet-statemine', 'wallet-statemint', 'westend']
+const project = ['wallet-acala', 'wallet-altair', 'wallet-astar', 'wallet-basilisk', 'wallet-bifrost', 'wallet-bit-country', 'wallet-calamari', 'wallet-clover', 'edgeware', 'wallet-karura', 'wallet-khala', 'wallet-kilt', 'kusama', 'wallet-moonbeam', 'wallet-moonriver', 'wallet-parallel', 'wallet-parallel-heiko', 'polkadot', 'wallet-quartz', 'wallet-statemine', 'wallet-statemint', 'westend', 'wallet-shiden']
 
 export default project
